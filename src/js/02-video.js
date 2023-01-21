7adcda8434a65933fd1dd6aa43b21a2de1c24445
Mark Zemlianskyi <mark080501@gmail.com>
2023-01-21 15:32:13 +0200

    debug of the path in packege json file

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,7 @@
+const iframe = document.querySelector('iframe');
+const player = new Video().Player(iframe);
 
+player.on('play', onPlay);
+function onPlay() {
+  console.log('playing the video');
+}
