890e7efce9aa8823ee053564538b569dfb8a1bac
Mark Zemlianskyi <mark080501@gmail.com>
2023-01-22 16:15:04 +0200

    task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,14 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+// console.log(_);
 const iframe = document.querySelector('iframe');
-const player = new Video().Player(iframe);
-
-player.on('play', onPlay);
-function onPlay() {
-  console.log('playing the video');
-}
+const player = new Player(iframe);
+const VIDEOLOCALPROGRESS_KEY = 'videoplayer-current-time';
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    console.log(data);
+    localStorage.setItem(VIDEOLOCALPROGRESS_KEY, data.seconds);
+  }, 1000)
+);
+player.setCurrentTime(localStorage.getItem(VIDEOLOCALPROGRESS_KEY));
