fc95868b60325ec39c3205647d951aa7c16da7eb
ZemMark <111459434+ZemMark@users.noreply.github.com>
2023-01-22 15:15:12 +0000

    Deploying to gh-pages from @ ZemMark/goit-js-hw-08@890e7efce9aa8823ee053564538b569dfb8a1bac 🚀

--- 02-video.915427fe.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe");(new Video).Player(e).on("play",(function(){console.log("playing the video")}));
-//# sourceMappingURL=02-video.915427fe.js.map
--- 02-video.f951bb20.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe");(new Video).Player(e).on("play",(function(){console.log("playing the video")}))}();
-//# sourceMappingURL=02-video.f951bb20.js.map
