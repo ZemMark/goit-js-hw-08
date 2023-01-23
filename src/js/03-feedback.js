bb182b15c01e1e60833f7c85d98141a7734ecc6f
Mark Zemlianskyi <mark080501@gmail.com>
2023-01-23 08:38:12 +0200

    debug packege json

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+const FEEDBACK_DATA = 'feedback-form-state';
+const refs = {
+  form: document.querySelector('form'),
+};
+const fd = {};
+let unparsedData = {};
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener(
+  'input',
+  throttle(e => {
+    fd[e.target.name] = e.target.value;
+    const preData = JSON.stringify(fd);
+    unparsedData = localStorage.setItem(FEEDBACK_DATA, JSON.parse(fd));
+  }, 1000)
+);
+dataRestor();
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK_DATA);
+}
 
+function dataRestor() {
+  const givenData = localStorage.getItem(FEEDBACK_DATA);
+  console.log(givenData);
+  // const parsedData = JSON.parse(givenData);
+  if (givenData) {
+    // refs.form.elements.email.value = parsedData.email;
+    // refs.form.elements.message.value = parsedData.message;
+  }
+  refs.form.elements.email.value = '';
+  refs.form.elements.message.value = '';
+}
